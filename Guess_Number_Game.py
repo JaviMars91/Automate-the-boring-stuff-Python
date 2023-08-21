51b434c33cce6895bbfc3d3354a06c0eadba3744
Javier Martinez <50782437+JaviMars91@users.noreply.github.com>
2023-08-21 00:12:45 +0000

    Input validation, Play_Game, and Play again

--- Guess_Number_Game.py
+++ Guess_Number_Game.py
@@ -1,23 +1,40 @@
-#Guess the number game -
 import random
-print('Hello, what is your name?')
-name = input()
-secretNumber = random.randint(1, 20)
-print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 
-# Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
-    print('Take a guess.')
-    guess = int(input())
-    if guess < secretNumber:
-        print('Your guess is too low.')
-    elif guess > secretNumber:
-        print('Your guess is too high.')
-    else:  # Use 'else' instead of 'Else'
-        break  # This condition is the correct guess!
+#added input validation
+def get_valid_input(prompt, data_type=int):
+    while True:
+        try:
+            user_input = data_type(input(prompt))
+            return user_input
+        except ValueError:
+            print("Invalid input. Please enter a valid value.")
 
-if guess == secretNumber:
-    print('Good job, ' + name + '! You guessed my number between 1 and 20.')
-else:  # Use 'else' instead of 'Else'
-    print('Nope. The number I was thinking of was ' + str(secretNumber))
+def play_game():
+    print("Hello, what is your name?")
+    name = input()
 
+    while True:
+        secretNumber = random.randint(1, 20)
+        print("Well, " + name + ", I am thinking of a number between 1 and 20.")
+
+        for guessesTaken in range(1, 7):
+            guess = get_valid_input("Take a guess: ")
+
+            if guess < secretNumber:
+                print("Your guess is too low.")
+            elif guess > secretNumber:
+                print("Your guess is too high.")
+            else:
+                print("Good job, " + name + "! You guessed my number between 1 and 20 in " + str(guessesTaken) + " guesses.")
+                break
+
+        if guess != secretNumber:
+            print("Nope. The number I was thinking of was " + str(secretNumber))
+#added play again option
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            print("Thanks for playing!")
+            break
+
+if __name__ == "__main__":
+    play_game()
